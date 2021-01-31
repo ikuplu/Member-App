341a0a317ca4a9e20a49b1658df639bd8cd922dc
Ismail Kuplu <i.kuplu@outlook.com>
2021-01-31 12:32:10 +0100

    Added route to get a single member object

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const  path = require('path');
+const path = require('path');
+const logger = require('./middleware/logger');
 const members = require('./Members');
 
 const app = express();
 
+// Init middleware
+// app.use(logger);
 
-// Gets the members
+// Gets all the members
 app.get('/api/members', (req, res) => {
   res.json(members);
+});
+
+// Get single member
+app.get('/api/members/:id', (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
