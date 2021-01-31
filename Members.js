8f8eb20b9b26e4b89b595197834f2b72d2c5b7e3
Ismail Kuplu <i.kuplu@outlook.com>
2021-01-31 11:26:54 +0100

    Added route for the members which imports them from an apart module

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Smith',
+    email: 'jsmith@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob Johnson',
+    email: 'bjohnson@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Elena Brown',
+    email: 'ebrown@gmail.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const  path = require('path');
+const members = require('./Members');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+
+// Gets the members
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
