68feb9e411ebce778118e9ccb6c195f1f13d0261
Ismail Kuplu <i.kuplu@outlook.com>
2021-02-02 13:29:54 +0100

    Rendered the members into the homepage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -17,10 +18,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Homepage Route
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members,
+  }),
+);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API routes
 app.use('/api/members', require('./routes/api/members'));
